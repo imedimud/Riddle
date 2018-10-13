5addd5b1e832a3200db6d0084694cfd81ac862cf
imedimud <44009808+imedimud@users.noreply.github.com>
2018-10-14 00:12:08 +1100

    Create answer.php

---
+++ answer.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+It is good to meet you, <?php echo $_GET["name"]; ?><br>
+My name is Tom Riddle
+What do you want to know?
+<form action="answer2.php" method="get">
+<input type="text" name="name2"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
